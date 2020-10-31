b76db64f79309463b81a11861a4a2efb831986ee
Puneet181184 <chemical.puneet@gmail.com>
2020-10-31 09:17:35 +0530

    added books_app

---
+++ books_app/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from books_app import views
+from django.urls import re_path,path
+app_name="books_app"
+urlpatterns=[
+  re_path(r'home/',views.home,name='home'),
+  
+  ]
+  
+
