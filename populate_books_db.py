4eb44a28350c9de2bf8d6b0399d5897b9f90b275
Puneet181184 <chemical.puneet@gmail.com>
2020-11-13 08:58:25 +0530

    added populate books script

---
+++ populate_books_db.py
@@ -0,0 +1,66 @@
+import os 
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","music_site.settings")
+django.setup()
+
+from books_app.models import books_db
+
+def add_entry(title,author,publisher,isbn,pages,year,price):
+	t=music_db.objects.get_or_create(title=title,author=author,publisher=publisher,isbn=isbn,pages=pages,year=year,price=price)[0]
+	t.save()
+    
+
+#add imports
+import csv
+
+#open csv file
+csvfile=open("books_titles.csv","r")
+reader=csv.DictReader(csvfile)
+title=[]
+author=[]
+publisher=[]
+isbn=[]
+pages=[]
+year=[]
+price=[]
+
+
+
+#read values from the csv file
+for row in reader:
+	#print(row["Track Title"])
+     title.append(row["Book Title"])
+     author.append(row["Author Name"])
+     publisher.append(row["Publisher"])
+     isbn.append(row["ISBN"])
+     pages.append(row["Pages"])
+     year.append(row["Year"])
+     price.append(row["Price"])
+
+
+
+#inserting values into the Table
+for i in range(0,len(title)):
+	add_entry(title[i],author[i],publisher[i],isbn[i],pages[i],year[i],price[i])
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
