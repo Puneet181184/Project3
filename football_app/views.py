0e5edfd6b0646b6f82f574638e67c3971cb21a0c
Puneet181184 <chemical.puneet@gmail.com>
2020-12-28 09:19:34 +0530

    added football player page

--- football_app/views.py
+++ football_app/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"football_app/home.html")
+def player(request):
+    players_list=football_db.objects.order_by("name")
+    football_dict={"player":players_list}
+    return render(request,"football_app/player.html",context=football_dict)		
+
