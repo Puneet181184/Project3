a0eb60d55619d803654643d0804e8fb3b5a12458
Puneet181184 <chemical.puneet@gmail.com>
2020-12-29 09:18:41 +0530

    added football_app model

--- football_app/models.py
+++ football_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class football_db(models.Model):
+   name=models.CharField(max_length=512,null=True)
+   age=models.CharField(max_length=512,null=True)
+   dob=models.CharField(max_length=512,null=True)
+   nationality=models.CharField(max_length=512,null=True)
+   birthplace=models.CharField(max_length=512,null=True)
+   fifaid=models.CharField(max_length=512,null=True)
+   debut=models.CharField(max_length=512,null=True)
+   position=models.CharField(max_length=512,null=True)
+   league=models.CharField(max_length=512,null=True)
+   club=models.CharField(max_length=512,null=True)
+   height=models.CharField(max_length=512,null=True)
+   weight=models.CharField(max_length=512,null=True)
+   foot=models.CharField(max_length=512,null=True)
+   matches=models.CharField(max_length=512,null=True)
+   goals=models.CharField(max_length=512,null=True)
+   assists=models.CharField(max_length=512,null=True)
+   penaltyscored=models.CharField(max_length=512,null=True)
+   penaltymissed=models.CharField(max_length=512,null=True)
+   goalsconcedeed=models.CharField(max_length=512,null=True)
+   yellowcards=models.CharField(max_length=512,null=True)
+   redcards=models.CharField(max_length=512,null=True)
+
+
+
+
+
+
--- football_app/views.py
+++ football_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from football_app.models import football_db
 
 
 def home(request):
