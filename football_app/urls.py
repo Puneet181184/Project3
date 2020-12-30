7cb384d17cb20b248ba15e002b69478e535b3f78
Puneet181184 <chemical.puneet@gmail.com>
2020-12-30 08:59:05 +0530

    added other pages for football_app

--- football_app/urls.py
+++ football_app/urls.py
@@ -5,6 +5,13 @@ app_name="football_app"
 urlpatterns=[
   re_path(r'home/',views.home,name='home'),
   re_path(r'player/',views.player,name='player'),
+  re_path(r'about/',views.about,name='about'),
+  re_path(r'details/',views.details,name='details'),
+  re_path(r'matchstats/',views.matchstats,name='matchstats'),
+  re_path(r'goalstats/',views.goalstats,name='goalstats'),
+  re_path(r'penaltystats/',views.penaltystats,name='penaltystats'),
+  re_path(r'cardstats/',views.cardstats,name='cardstats'),
+  
   ]
   
 
--- football_app/views.py
+++ football_app/views.py
@@ -9,4 +9,38 @@ def player(request):
     players_list=football_db.objects.order_by("name")
     football_dict={"player":players_list}
     return render(request,"football_app/player.html",context=football_dict)		
+def about(request):
+    players_list=football_db.objects.order_by("name")
+    football_dict={"player":players_list}
+    return render(request,"football_app/about.html",context=football_dict)
+def details(request):
+    players_list=football_db.objects.order_by("name")
+    football_dict={"player":players_list}
+    return render(request,"football_app/details.html",context=football_dict)
+def matchstats(request):
+    players_list=football_db.objects.order_by("name")
+    football_dict={"player":players_list}
+    return render(request,"football_app/matchstats.html",context=football_dict)
+def goalstats(request):
+    players_list=football_db.objects.order_by("name")
+    football_dict={"player":players_list}
+    return render(request,"football_app/goalstats.html",context=football_dict)
+def penaltystats(request):
+    players_list=football_db.objects.order_by("name")
+    football_dict={"player":players_list}
+    return render(request,"football_app/penaltystats.html",context=football_dict)
+def cardstats(request):
+    players_list=football_db.objects.order_by("name")
+    football_dict={"player":players_list}
+    return render(request,"football_app/cardstats.html",context=football_dict)		
+
+
+
+    		
+
+
+
+
+
+
 
