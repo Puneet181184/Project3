7788e8ee967f34f3860770167b429b20195e9dd1
Puneet181184 <chemical.puneet@gmail.com>
2021-01-29 08:31:58 +0530

    added other views for tennis

--- tennis_app/urls.py
+++ tennis_app/urls.py
@@ -5,7 +5,15 @@ app_name="tennis_app"
 urlpatterns=[
   re_path(r'home/',views.home,name='home'),
   re_path(r'player/',views.player,name='player'),
-  
+  re_path(r'about/',views.about,name='about'),
+  re_path(r'details/',views.details,name='details'),
+  re_path(r'careerstats/',views.careerstats,name='careerstats'),
+  re_path(r'pointstats/',views.pointstats,name='pointstats'),
+  re_path(r'winstats/',views.winstats,name='winstats'),
+  re_path(r'gamestats/',views.gamestats,name='gamestats'),
+
+
  ]
   
+
 
--- tennis_app/views.py
+++ tennis_app/views.py
@@ -7,3 +7,31 @@ def player(request):
     players_list=tennis_db.objects.order_by("name")
     tennis_dict={"player":players_list}
     return render(request,"tennis_app/player.html",context=tennis_dict)		
+def about(request):
+    players_list=tennis_db.objects.order_by("name")
+    tennis_dict={"player":players_list}
+    return render(request,"tennis_app/about.html",context=tennis_dict)
+def details(request):
+    players_list=tennis_db.objects.order_by("name")
+    tennis_dict={"player":players_list}
+    return render(request,"tennis_app/details.html",context=tennis_dict)
+def careerstats(request):
+    players_list=tennis_db.objects.order_by("name")
+    tennis_dict={"player":players_list}
+    return render(request,"tennis_app/careerstats.html",context=tennis_dict)
+def pointstats(request):
+    players_list=tennis_db.objects.order_by("name")
+    tennis_dict={"player":players_list}
+    return render(request,"tennis_app/pointstats.html",context=tennis_dict)		
+def winstats(request):
+    players_list=tennis_db.objects.order_by("name")
+    tennis_dict={"player":players_list}
+    return render(request,"tennis_app/winstats.html",context=tennis_dict)
+def gamestats(request):
+    players_list=tennis_db.objects.order_by("name")
+    tennis_dict={"player":players_list}
+    return render(request,"tennis_app/gamestats.html",context=tennis_dict)		
+
+
+
+
