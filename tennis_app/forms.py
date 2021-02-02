2fa2d8f848a9fb3be859c5a549113f0408a361d3
Puneet181184 <chemical.puneet@gmail.com>
2021-02-02 08:42:59 +0530

    added player html page

--- tennis_app/forms.py
+++ tennis_app/forms.py
@@ -5,4 +5,35 @@ class playerform(forms.ModelForm):
       class Meta():
          model=tennis_db
          fields=["name","age","dob","nationality"]
-         
+
+class aboutform(forms.ModelForm):
+      class Meta():
+         model=tennis_db
+         fields=["name","rank","debut"]
+
+class detailsform(forms.ModelForm):
+      class Meta():
+         model=tennis_db
+         fields=["name","seasons","height","weight","preferedhand"]
+
+class careerstatsform(forms.ModelForm):
+      class Meta():
+         model=tennis_db
+         fields=["name","titles","grandslams","tourfinals","masters","daviscups"]
+
+class pointstatsform(forms.ModelForm):
+      class Meta():
+         model=tennis_db
+         fields=["name","totalpoints","tournamentpoints","rankingpoints"]
+
+class winstatsform(forms.ModelForm):
+      class Meta():
+         model=tennis_db
+         fields=["name","overallwins","hardwins","claywins","grasswins","carpetwins"]
+
+class gamestatsform(forms.ModelForm):
+      class Meta():
+         model=tennis_db
+         fields=["name","games","aces","faults"]
+
+                                             
