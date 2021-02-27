082adcac036b041afe1cda56a42c9efbb6e02b4a
Puneet181184 <chemical.puneet@gmail.com>
2021-02-27 08:56:12 +0530

    added other functions for chess_app

--- chess_app/urls.py
+++ chess_app/urls.py
@@ -5,4 +5,13 @@ app_name="chess_app"
 urlpatterns=[
   re_path(r'home/',views.home,name='home'),
   re_path(r'player/',views.player,name='player'),
+  re_path(r'about/',views.about,name='about'),
+  re_path(r'details/',views.details,name='details'),
+  re_path(r'personalstats/',views.personalstats,name='personalstats'),
+  re_path(r'ratingstats/',views.ratingstats,name='ratingstats'),
+  re_path(r'gamestats/',views.gamestats,name='gamestats'),
+
+  
+
+
 ]  
--- chess_app/views.py
+++ chess_app/views.py
@@ -8,4 +8,23 @@ def player(request):
     players_list=chess_db.objects.order_by("name")
     chess_dict={"player":players_list}
     return render(request,"chess_app/player.html",context=chess_dict)		
-	
+def about(request):
+    players_list=chess_db.objects.order_by("name")
+    chess_dict={"player":players_list}
+    return render(request,"chess_app/about.html",context=chess_dict)		
+def details(request):
+    players_list=chess_db.objects.order_by("name")
+    chess_dict={"player":players_list}
+    return render(request,"chess_app/details.html",context=chess_dict)		
+def personalstats(request):
+    players_list=chess_db.objects.order_by("name")
+    chess_dict={"player":players_list}
+    return render(request,"chess_app/personalstats.html",context=chess_dict)		
+def ratingstats(request):
+    players_list=chess_db.objects.order_by("name")
+    chess_dict={"player":players_list}
+    return render(request,"chess_app/ratingstats.html",context=chess_dict)
+def gamestats(request):
+    players_list=chess_db.objects.order_by("name")
+    chess_dict={"player":players_list}
+    return render(request,"chess_app/gamestats.html",context=chess_dict)	    		
