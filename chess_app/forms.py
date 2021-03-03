db527c053e14e9ff495ffb84db01553806bf9112
Puneet181184 <chemical.puneet@gmail.com>
2021-03-03 09:24:06 +0530

    added form pages for others

--- chess_app/forms.py
+++ chess_app/forms.py
@@ -5,3 +5,24 @@ class playerform(forms.ModelForm):
       class Meta():
          model=chess_db
          fields=["name","age","dob","nationality"]
+class aboutform(forms.ModelForm):
+      class Meta():
+         model=chess_db
+         fields=["rank","debut"]
+class detailsform(forms.ModelForm):
+      class Meta():
+         model=chess_db
+         fields=["height","weight"] 
+class personalstatsform(forms.ModelForm):
+      class Meta():
+         model=chess_db
+         fields=["title","playerid","gender"]
+class ratingstatsform(forms.ModelForm):
+      class Meta():
+         model=chess_db
+         fields=["stdrating","rapidrating","blitzrating"]
+class gamestatsform(forms.ModelForm):
+      class Meta():
+         model=chess_db
+         fields=["games","wins","draws","losses","score"]                                   
+         
--- chess_app/views.py
+++ chess_app/views.py
@@ -44,4 +44,79 @@ def form_player(request):
          return render(request,"chess_app/submit_player.html")
       else:
          print("error form invalid")      
-    return render(request,"chess_app/form_player.html",{"form":form})    	    		
+    return render(request,"chess_app/form_player.html",{"form":form})  
+def form_about(request):
+    form=aboutform()
+    if request.method=="POST":
+      form=aboutform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         rank=form.cleaned_data["rank"]
+         debut=form.cleaned_data["debut"]
+         defaults={"rank":rank,"debut":debut}
+         obj,created=chess_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"chess_app/submit_about.html")
+      else:
+         print("error form invalid")      
+    return render(request,"chess_app/form_about.html",{"form":form})  
+def form_details(request):
+    form=detailsform()
+    if request.method=="POST":
+      form=detailsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         height=form.cleaned_data["height"]
+         weight=form.cleaned_data["weight"]
+         defaults={"height":height,"weight":weight}
+         obj,created=chess_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"chess_app/submit_details.html")
+      else:
+         print("error form invalid")      
+    return render(request,"chess_app/form_details.html",{"form":form}) 
+def form_personalstats(request):
+    form=personalstatsform()
+    if request.method=="POST":
+      form=personalstatsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         title=form.cleaned_data["title"]
+         playerid=form.cleaned_data["playerid"]
+         gender=form.cleaned_data["gender"]
+         defaults={"title":age,"playerid":playerid,"gender":gender}
+         obj,created=chess_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"chess_app/submit_personalstats.html")
+      else:
+         print("error form invalid")      
+    return render(request,"chess_app/form_personalstats.html",{"form":form}) 
+def form_ratingstats(request):
+    form=ratingstatsform()
+    if request.method=="POST":
+      form=ratingstatsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         stdrating=form.cleaned_data["stdrating"]
+         rapidrating=form.cleaned_data["rapidrating"]
+         blitzrating=form.cleaned_data["blitzrating"]
+         defaults={"stdrating":stdrating,"rapidrating":rapidrating,"blitzrating":blitzrating}
+         obj,created=chess_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"chess_app/submit_ratingstats.html")
+      else:
+         print("error form invalid")      
+    return render(request,"chess_app/form_ratingstats.html",{"form":form}) 
+def form_gamestats(request):
+    form=gamestatsform()
+    if request.method=="POST":
+      form=gamestatsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         games=form.cleaned_data["games"]
+         wins=form.cleaned_data["wins"]
+         draws=form.cleaned_data["draws"]
+         losses=form.cleaned_data["losses"]
+         score=form.cleaned_data["score"]
+         defaults={"games":games,"wins":wins,"draws":draws,"losses":losses,"score":score}
+         obj,created=chess_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"chess_app/submit_gamestats.html")
+      else:
+         print("error form invalid")      
+    return render(request,"chess_app/form_gamestats.html",{"form":form})                 	    		
