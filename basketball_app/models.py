17d5f73c49313a0beef7b5a346c7c4c5e0e17ec4
Puneet181184 <chemical.puneet@gmail.com>
2021-03-24 09:01:07 +0530

    added player function for basketball app

--- basketball_app/models.py
+++ basketball_app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+
 # Create your models here.
+class  basketball_db(models.Model):
+   name=models.CharField(max_length=512,null=True)
+   age=models.CharField(max_length=512,null=True)
+   dob=models.CharField(max_length=512,null=True)
+   nationality=models.CharField(max_length=512,null=True)
+   college=models.CharField(max_length=512,null=True)
+   position=models.CharField(max_length=512,null=True)
+   height=models.CharField(max_length=512,null=True)
+   weight=models.CharField(max_length=512,null=True)
+   preferedhand=models.CharField(max_length=512,null=True)
+   games=models.CharField(max_length=512,null=True)
+   gamestarted=models.CharField(max_length=512,null=True)
+   minutes=models.CharField(max_length=512,null=True)
+   goals=models.CharField(max_length=512,null=True)
+   attempts=models.CharField(max_length=512,null=True)
+   assists=models.CharField(max_length=512,null=True)
+   steals=models.CharField(max_length=512,null=True)
+   points=models.CharField(max_length=512,null=True)
+   threepoints=models.CharField(max_length=512,null=True)
+   twopoints=models.CharField(max_length=512,null=True)
+   blocks=models.CharField(max_length=512,null=True)
+   freethrows=models.CharField(max_length=512,null=True)
+   rebounds=models.CharField(max_length=512,null=True)
+   fouls=models.CharField(max_length=512,null=True)
+
+
+
+
+
+
+
+
+
+
+
--- basketball_app/views.py
+++ basketball_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from basketball_app.models import basketball_db
 
 # Create your views here.
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"basketball_app/home.html")
+def player(request):
+    players_list=basketball_db.objects.order_by("name")
+    basketball_dict={"player":players_list}
+    return render(request,"basketball_app/player.html",context=basketball_dict)		
