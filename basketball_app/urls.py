dad2abf428366ebe5b6d3116c73fa0640ba87bb7
Puneet181184 <chemical.puneet@gmail.com>
2021-03-26 08:52:37 +0530

    added other views for basketball

--- basketball_app/urls.py
+++ basketball_app/urls.py
@@ -5,4 +5,11 @@ app_name="basketball_app"
 urlpatterns=[
   re_path(r'home/',views.home,name='home'),
   re_path(r'player/',views.player,name='player'),
+  re_path(r'about/',views.about,name='about'),
+  re_path(r'details/',views.details,name='details'),
+  re_path(r'gamestats/',views.gamestats,name='gamestats'),
+  re_path(r'goalstats/',views.goalstats,name='goalstats'),
+  re_path(r'pointstats/',views.pointstats,name='pointstats'),
+  re_path(r'blockstats/',views.blockstats,name='blockstats'),
+  
   ]
--- basketball_app/views.py
+++ basketball_app/views.py
@@ -9,3 +9,33 @@ def player(request):
     players_list=basketball_db.objects.order_by("name")
     basketball_dict={"player":players_list}
     return render(request,"basketball_app/player.html",context=basketball_dict)		
+def about(request):
+    players_list=basketball_db.objects.order_by("name")
+    basketball_dict={"player":players_list}
+    return render(request,"basketball_app/about.html",context=basketball_dict)
+def details(request):
+    players_list=basketball_db.objects.order_by("name")
+    basketball_dict={"player":players_list}
+    return render(request,"basketball_app/details.html",context=basketball_dict)
+def gamestats(request):
+    players_list=basketball_db.objects.order_by("name")
+    basketball_dict={"player":players_list}
+    return render(request,"basketball_app/gamestats.html",context=basketball_dict)
+def goalstats(request):
+    players_list=basketball_db.objects.order_by("name")
+    basketball_dict={"player":players_list}
+    return render(request,"basketball_app/goalstats.html",context=basketball_dict)
+def pointstats(request):
+    players_list=basketball_db.objects.order_by("name")
+    basketball_dict={"player":players_list}
+    return render(request,"basketball_app/pointstats.html",context=basketball_dict)
+def blockstats(request):
+    players_list=basketball_db.objects.order_by("name")
+    basketball_dict={"player":players_list}
+    return render(request,"basketball_app/blockstats.html",context=basketball_dict)		
+
+
+
+
+
+
