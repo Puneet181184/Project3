862b0b33c203b98c6d87a1d84414e58cdba024b6
Puneet181184 <chemical.puneet@gmail.com>
2021-04-15 09:03:46 +0530

    added contactus page

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,8 @@ from main import views
 from django.urls import re_path,path
 app_name="main"
 urlpatterns=[
-re_path(r'',views.main,name='main'),
+re_path(r'home',views.main,name='main'),
+re_path(r'contactus',views.contactus,name="contactus")
   ]
   
 
--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def main(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"home.html")	
+def contactus(request):
+	#return HttpResponse("Hello World!")
+	return render(request,"contactus.html")		
