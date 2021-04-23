eda7cfeab66ab1708a5a4bd36afea60f47ddf22c
Puneet181184 <chemical.puneet@gmail.com>
2021-04-23 08:50:46 +0530

    added other functions for baseball app

--- baseball_app/urls.py
+++ baseball_app/urls.py
@@ -5,4 +5,18 @@ app_name="baseball_app"
 urlpatterns=[
   re_path(r'home/',views.home,name='home'),
   re_path(r'player/',views.player,name='player'),
+  re_path(r'about/',views.about,name='about'),
+  re_path(r'details/',views.details,name='details'),
+  re_path(r'gamestats/',views.gamestats,name='gamestats'),
+  re_path(r'runstats/',views.runstats,name='runstats'),
+  re_path(r'strikestats/',views.strikestats,name='strikestats'),
+  re_path(r'basestats/',views.basestats,name='basestats'),
+  
+  
+  
+  
+  
+  
+  
+  
   ]
--- baseball_app/views.py
+++ baseball_app/views.py
@@ -9,6 +9,30 @@ def player(request):
     players_list=baseball_db.objects.order_by("name")
     baseball_dict={"player":players_list}
     return render(request,"baseball_app/player.html",context=baseball_dict)			
-
+def about(request):
+    players_list=baseball_db.objects.order_by("name")
+    baseball_dict={"player":players_list}
+    return render(request,"baseball_app/about.html",context=baseball_dict)
+def details(request):
+    players_list=baseball_db.objects.order_by("name")
+    baseball_dict={"player":players_list}
+    return render(request,"baseball_app/details.html",context=baseball_dict)
+def gamestats(request):
+    players_list=baseball_db.objects.order_by("name")
+    baseball_dict={"player":players_list}
+    return render(request,"baseball_app/gamestats.html",context=baseball_dict)
+def runstats(request):
+    players_list=baseball_db.objects.order_by("name")
+    baseball_dict={"player":players_list}
+    return render(request,"baseball_app/runstats.html",context=baseball_dict)
+def strikestats(request):
+    players_list=baseball_db.objects.order_by("name")
+    baseball_dict={"player":players_list}
+    return render(request,"baseball_app/strikestats.html",context=baseball_dict)
+def basestats(request):
+    players_list=baseball_db.objects.order_by("name")
+    baseball_dict={"player":players_list}
+    return render(request,"baseball_app/basestats.html",context=baseball_dict)
+    		    		    		    		    		    		
 
 
