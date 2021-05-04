ebde07dcc4034b08775efe1db93927b80109ecad
Puneet181184 <chemical.puneet@gmail.com>
2021-05-04 08:49:42 +0530

    added form html files for baseball app

--- baseball_app/urls.py
+++ baseball_app/urls.py
@@ -12,11 +12,11 @@ urlpatterns=[
   re_path(r'strikestats/',views.strikestats,name='strikestats'),
   re_path(r'basestats/',views.basestats,name='basestats'),
   re_path(r'form_p/',views.form_player,name='form_player'),
-  
-  
-  
-  
-  
-  
+  re_path(r'form_a/',views.form_about,name='form_about'),
+  re_path(r'form_d/',views.form_details,name='form_details'),
+  re_path(r'form_g/',views.form_gamestats,name='form_gamestats'),
+  re_path(r'form_r/',views.form_runstats,name='form_runstats'),
+  re_path(r'form_s/',views.form_strikestats,name='form_strikestats'),
+  re_path(r'form_b/',views.form_basestats,name='form_basestats'),
   
   ]
