9c5a8271e28d2272efd1349ec991cc448999b156
Puneet181184 <chemical.puneet@gmail.com>
2021-05-25 08:55:43 +0530

    added other pages for rugby app

--- rugby_app/urls.py
+++ rugby_app/urls.py
@@ -5,4 +5,16 @@ app_name="rugby_app"
 urlpatterns=[
   re_path(r'home/',views.home,name='home'),
   re_path(r'player/',views.player,name='player'),
+  re_path(r'about/',views.about,name='about'),
+  re_path(r'details/',views.details,name='details'),
+  re_path(r'matchstats/',views.matchstats,name='matchstats'),
+  re_path(r'gamestats/',views.gamestats,name='gamestats'),
+  re_path(r'pointstats/',views.pointstats,name='pointstats'),
+  re_path(r'cardstats/',views.cardstats,name='cardstats'),
+
+
+
+
+
+
   ]
--- rugby_app/views.py
+++ rugby_app/views.py
@@ -8,4 +8,27 @@ def player(request):
     players_list=rugby_db.objects.order_by("name")
     rugby_dict={"player":players_list}
     return render(request,"rugby_app/player.html",context=rugby_dict)			
-	
+def about(request):
+    players_list=rugby_db.objects.order_by("name")
+    rugby_dict={"player":players_list}
+    return render(request,"rugby_app/about.html",context=rugby_dict)		
+def details(request):
+    players_list=rugby_db.objects.order_by("name")
+    rugby_dict={"player":players_list}
+    return render(request,"rugby_app/details.html",context=rugby_dict)
+def matchstats(request):
+    players_list=rugby_db.objects.order_by("name")
+    rugby_dict={"player":players_list}
+    return render(request,"rugby_app/matchstats.html",context=rugby_dict)
+def gamestats(request):
+    players_list=rugby_db.objects.order_by("name")
+    rugby_dict={"player":players_list}
+    return render(request,"rugby_app/gamestats.html",context=rugby_dict)
+def pointstats(request):
+    players_list=rugby_db.objects.order_by("name")
+    rugby_dict={"player":players_list}
+    return render(request,"rugby_app/pointstats.html",context=rugby_dict)
+def cardstats(request):
+    players_list=rugby_db.objects.order_by("name")
+    rugby_dict={"player":players_list}
+    return render(request,"rugby_app/cardstats.html",context=rugby_dict)	    	    	    	    	
