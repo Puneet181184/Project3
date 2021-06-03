a055bf3d3403da32b0af578ede1da57be29ba7f9
Puneet181184 <chemical.puneet@gmail.com>
2021-06-03 09:18:36 +0530

    added form functions for rugby app

--- rugby_app/views.py
+++ rugby_app/views.py
@@ -48,4 +48,160 @@ def form_player(request):
          return render(request,"rugby_app/submit_player.html")
       else:
          print("error form invalid")      
-    return render(request,"rugby_app/form_player.html",{"form":form})       	    	    	    	    	
+    return render(request,"rugby_app/form_player.html",{"form":form})
+def form_about(request):
+    form=aboutform()
+    if request.method=="POST":
+      form=aboutform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         team=form.cleaned_data["team"]
+         position=form.cleaned_data["position"]
+         defaults={"team":team,"position"position}
+         obj,created=rugby_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"rugby_app/submit_about.html")
+      else:
+         print("error form invalid")      
+    return render(request,"rugby_app/form_about.html",{"form":form})
+def form_details(request):    
+    form=detailsform()
+    if request.method=="POST":
+      form=detailsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         debut=form.cleaned_data["debut"]
+         height=form.cleaned_data["height"]
+         weight=form.cleaned_data["weight"]
+         defaults={"debut":debut,"height":height,"weight":weight}
+         obj,created=rugby_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"rugby_app/submit_details.html")
+      else:
+         print("error form invalid")  
+    return render(request,"rugby_app/form_details.html",{"form":form}) 
+def form_matchstats(request):    
+    form=matchstatsform()
+    if request.method=="POST":
+      form=matchstatsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         matches=form.cleaned_data["matches"]
+         wins=form.cleaned_data["wins"]
+         draws=form.cleaned_data["drwas"]
+         losses=form.cleaned_data["losses"]
+         defaults={"matches":matches,"wins":wins,"draws":draws,"losses":losses}
+         obj,created=rugby_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"rugby_app/submit_matchstats.html")
+      else:
+         print("error form invalid")  
+    return render(request,"rugby_app/form_matchstats.html",{"form":form})
+def form_gamestats(request):    
+    form=gamestatsform()
+    if request.method=="POST":
+      form=gamestatsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         minutes=form.cleaned_data["minutes"]
+         started=form.cleaned_data["started"]
+         defaults={"minutes":minutes,"started":started}
+         obj,created=rugby_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"rugby_app/submit_gamestats.html")
+      else:
+         print("error form invalid")  
+    return render(request,"rugby_app/form_gamestats.html",{"form":form})
+def form_pointstats(request):    
+    form=pointstatsform()
+    if request.method=="POST":
+      form=pointstatsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         points=form.cleaned_data["points"]
+         tries=form.cleaned_data["tries"]
+         drops=form.cleaned_data["drops"]
+         penalties=form.cleaned_data["penalties"]
+         conversions=form.cleaned_data["conversions"]
+         defaults={"points":points,"tries":tries,"drops":drops,"penalties":penalties,"conversions":conversions}
+         obj,created=rugby_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"rugby_app/submit_pointstats.html")
+      else:
+         print("error form invalid")  
+    return render(request,"rugby_app/form_pointstats.html",{"form":form}) 
+def form_cardstats(request):    
+    form=cardstatsform()
+    if request.method=="POST":
+      form=cardstatsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         yellowcards=form.cleaned_data["yellowcards"]
+         redcards=form.cleaned_data["redcards"]
+         defaults={"yellowcards":yellowcards,"redcards":redcards}
+         obj,created=rugby_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"rugby_app/submit_cardstats.html")
+      else:
+         print("error form invalid")  
+    return render(request,"rugby_app/form_cardstats.html",{"form":form}) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+       	    	    	    	    	          	    	        	    	    	    	    	          	    	    	    	    	          	    	    	    	    	           	    	    	    	    	
