cd8d317b43f5d37b69ab03a456bada5a7816d929
Puneet181184 <chemical.puneet@gmail.com>
2021-06-30 08:48:27 +0530

    added player function for icehockey app

--- icehockey_app/models.py
+++ icehockey_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+
 # Create your models here.
+class  icehockey_db(models.Model):
+   name=models.CharField(max_length=512,null=True)
+   age=models.CharField(max_length=512,null=True)
+   dob=models.CharField(max_length=512,null=True)
+   nationality=models.CharField(max_length=512,null=True)
+   team=models.CharField(max_length=512,null=True)
+   position=models.CharField(max_length=512,null=True)
+   debut=models.CharField(max_length=512,null=True)
+   height=models.CharField(max_length=512,null=True)
+   weight=models.CharField(max_length=512,null=True)
+   games=models.CharField(max_length=512,null=True)
+   assists=models.CharField(max_length=512,null=True)
+   points=models.CharField(max_length=512,null=True)
+   penaltyminutes=models.CharField(max_length=512,null=True)
+   goals=models.CharField(max_length=512,null=True)
+   powerplaygoals=models.CharField(max_length=512,null=True)
+   shorthandgoals=models.CharField(max_length=512,null=True)
+   powerplaypoints=models.CharField(max_length=512,null=True)
+   shorthandpoints=models.CharField(max_length=512,null=True)
+   wininggoals=models.CharField(max_length=512,null=True)
+   overtimegoals=models.CharField(max_length=512,null=True)
+   shots=models.CharField(max_length=512,null=True)
+   shotspercentage=models.CharField(max_length=512,null=True)
+   winspercentage=models.CharField(max_length=512,null=True)
+   
+
+
+
+
+
+
--- icehockey_app/views.py
+++ icehockey_app/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from icehockey_app.models import icehockey_db
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"icehockey_app/home.html")
+def player(request):
+    players_list=icehockey_db.objects.order_by("name")
+    icehockey_dict={"player":players_list}
+    return render(request,"icehockey_app/player.html",context=icehockey_dict)		
+
