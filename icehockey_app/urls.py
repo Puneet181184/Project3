b612fc291dba400a29b4d6d15bd886451b32d39e
Puneet181184 <chemical.puneet@gmail.com>
2021-07-02 08:53:04 +0530

    added functions for icehockey app

--- icehockey_app/urls.py
+++ icehockey_app/urls.py
@@ -5,5 +5,10 @@ app_name="icehockey_app"
 urlpatterns=[
   re_path(r'home/',views.home,name='home'),
   re_path(r'player/',views.player,name='player'),
-  
+  re_path(r'about/',views.about,name='about'),
+  re_path(r'details/',views.details,name='details'),
+  re_path(r'gamestats/',views.gamestats,name='gamestats'),
+  re_path(r'goalstats/',views.goalstats,name='goalstats'),
+  re_path(r'pointstats/',views.pointstats,name='pointstats'),
+  re_path(r'shotstats/',views.shotstats,name='shotstats'),
   ]
--- icehockey_app/views.py
+++ icehockey_app/views.py
@@ -7,4 +7,38 @@ def player(request):
     players_list=icehockey_db.objects.order_by("name")
     icehockey_dict={"player":players_list}
     return render(request,"icehockey_app/player.html",context=icehockey_dict)		
+def about(request):
+    players_list=icehockey_db.objects.order_by("name")
+    icehockey_dict={"player":players_list}
+    return render(request,"icehockey_app/about.html",context=icehockey_dict)       
+def details(request):
+    players_list=icehockey_db.objects.order_by("name")
+    icehockey_dict={"player":players_list}
+    return render(request,"icehockey_app/details.html",context=icehockey_dict) 
+def gamestats(request):
+    players_list=icehockey_db.objects.order_by("name")
+    icehockey_dict={"player":players_list}
+    return render(request,"icehockey_app/gamestats.html",context=icehockey_dict)
+def goalstats(request):
+    players_list=icehockey_db.objects.order_by("name")
+    icehockey_dict={"player":players_list}
+    return render(request,"icehockey_app/goalstats.html",context=icehockey_dict)
+def pointstats(request):
+    players_list=icehockey_db.objects.order_by("name")
+    icehockey_dict={"player":players_list}
+    return render(request,"icehockey_app/pointstats.html",context=icehockey_dict)
+def shotstats(request):
+    players_list=icehockey_db.objects.order_by("name")
+    icehockey_dict={"player":players_list}
+    return render(request,"icehockey_app/shotstats.html",context=icehockey_dict)       
+
+
+
+
+
+
+
+
+
+
 
