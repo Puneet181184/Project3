5e49638583fe87d1f1995bee2862dea6f1a9d656
Puneet181184 <chemical.puneet@gmail.com>
2021-08-03 08:52:56 +0530

    added other functions for golf_app

--- golf_app/urls.py
+++ golf_app/urls.py
@@ -5,5 +5,14 @@ app_name="golf_app"
 urlpatterns=[
   re_path(r'home/',views.home,name='home'),
   re_path(r'player/',views.player,name='player'),
+  re_path(r'about/',views.about,name='about'),
+  re_path(r'details/',views.details,name='details'),
+  re_path(r'gamestats/',views.gamestats,name='gamestats'),
+  re_path(r'positionstats/',views.positionstats,name='positionstats'),
+  re_path(r'pointstats/',views.pointstats,name='pointstats'),
+  
+  
+  
+  
   
  ] 
--- golf_app/views.py
+++ golf_app/views.py
@@ -9,3 +9,23 @@ def player(request):
     players_list=golf_db.objects.order_by("name")
     golf_dict={"player":players_list}
     return render(request,"golf_app/player.html",context=golf_dict)			
+def about(request):
+    players_list=golf_db.objects.order_by("name")
+    golf_dict={"player":players_list}
+    return render(request,"golf_app/about.html",context=golf_dict)     
+def details(request):
+    players_list=golf_db.objects.order_by("name")
+    golf_dict={"player":players_list}
+    return render(request,"golf_app/details.html",context=golf_dict)
+def gamestats(request):
+    players_list=golf_db.objects.order_by("name")
+    golf_dict={"player":players_list}
+    return render(request,"golf_app/gamestats.html",context=golf_dict) 
+def positionstats(request):
+    players_list=golf_db.objects.order_by("name")
+    golf_dict={"player":players_list}
+    return render(request,"golf_app/positionstats.html",context=golf_dict) 
+def pointstats(request):
+    players_list=golf_db.objects.order_by("name")
+    golf_dict={"player":players_list}
+    return render(request,"golf_app/pointstats.html",context=golf_dict)                                  
