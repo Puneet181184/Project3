5763122a7e31f63c9d2d4afd62a9cf417c148b40
Puneet181184 <chemical.puneet@gmail.com>
2021-09-01 08:55:05 +0530

    added models file for badminton app

--- badminton_app/models.py
+++ badminton_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+class  badminton_db(models.Model):
+   name=models.CharField(max_length=512,null=True)
+   age=models.CharField(max_length=512,null=True)
+   dob=models.CharField(max_length=512,null=True)
+   country=models.CharField(max_length=512,null=True)
+   gender=models.CharField(max_length=512,null=True)
+   rank=models.CharField(max_length=512,null=True)
+   tourrank=models.CharField(max_length=512,null=True)
+   debut=models.CharField(max_length=512,null=True)
+   height=models.CharField(max_length=512,null=True)
+   weight=models.CharField(max_length=512,null=True)
+   plays=models.CharField(max_length=512,null=True)
+   careerwins=models.CharField(max_length=512,null=True)
+   singlesplayed=models.CharField(max_length=512,null=True)
+   singleswon=models.CharField(max_length=512,null=True)
+   singleslost=models.CharField(max_length=512,null=True)
+   doublesplayed=models.CharField(max_length=512,null=True)
+   doubleswon=models.CharField(max_length=512,null=True)
+   doubleslost=models.CharField(max_length=512,null=True)
+   mixedplayed=models.CharField(max_length=512,null=True)
+   mixedwon=models.CharField(max_length=512,null=True)
+   mixedlost=models.CharField(max_length=512,null=True)
+   
+   
+
--- badminton_app/views.py
+++ badminton_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from badminton_app.models import badminton_db
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"badminton_app/home.html")
+def player(request):
+    players_list=badminton_db.objects.order_by("name")
+    badminton_dict={"player":players_list}
+    return render(request,"badminton_app/player.html",context=badminton_dict)		
+
