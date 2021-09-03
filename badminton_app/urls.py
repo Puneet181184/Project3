777d062c9f8fa32e66ea8217f3b1636934361871
Puneet181184 <chemical.puneet@gmail.com>
2021-09-03 08:58:20 +0530

    added other functions for badminton app

--- badminton_app/urls.py
+++ badminton_app/urls.py
@@ -5,4 +5,12 @@ app_name="badminton_app"
 urlpatterns=[
   re_path(r'home/',views.home,name='home'),
   re_path(r'player/',views.player,name='player'),
-  ]
+  re_path(r'about/',views.about,name='about'),
+  re_path(r'details/',views.details,name='details'),
+  re_path(r'careerstats/',views.careerstats,name='careerstats'),
+  re_path(r'singlesstats/',views.singlesstats,name='singlesstats'),
+  re_path(r'doublesstats/',views.doublesstats,name='doublesstats'),
+  re_path(r'mixedstats/',views.mixedstats,name='mixedstats'),
+
+
+ ]
--- badminton_app/views.py
+++ badminton_app/views.py
@@ -7,4 +7,27 @@ def player(request):
     players_list=badminton_db.objects.order_by("name")
     badminton_dict={"player":players_list}
     return render(request,"badminton_app/player.html",context=badminton_dict)		
-
+def about(request):
+    players_list=badminton_db.objects.order_by("name")
+    badminton_dict={"player":players_list}
+    return render(request,"badminton_app/about.html",context=badminton_dict)
+def details(request):
+    players_list=badminton_db.objects.order_by("name")
+    badminton_dict={"player":players_list}
+    return render(request,"badminton_app/detailsr.html",context=badminton_dict)
+def careerstats(request):
+    players_list=badminton_db.objects.order_by("name")
+    badminton_dict={"player":players_list}
+    return render(request,"badminton_app/careerstats.html",context=badminton_dict)
+def singlesstats(request):
+    players_list=badminton_db.objects.order_by("name")
+    badminton_dict={"player":players_list}
+    return render(request,"badminton_app/singlesstats.html",context=badminton_dict) 
+def doublesstats(request):
+    players_list=badminton_db.objects.order_by("name")
+    badminton_dict={"player":players_list}
+    return render(request,"badminton_app/doublesstats.html",context=badminton_dict)
+def mixedstats(request):
+    players_list=badminton_db.objects.order_by("name")
+    badminton_dict={"player":players_list}
+    return render(request,"badminton_app/mixedstatsstats.html",context=badminton_dict)               
