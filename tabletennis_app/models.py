6f2efed1355c2b17fc84413b99dd831abb0d3df4
Puneet181184 <chemical.puneet@gmail.com>
2021-10-06 08:51:40 +0530

    added models.py for table tennis app

--- tabletennis_app/models.py
+++ tabletennis_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class  tabletennis_db(models.Model):
+   name=models.CharField(max_length=512,null=True)
+   age=models.CharField(max_length=512,null=True)
+   dob=models.CharField(max_length=512,null=True)
+   country=models.CharField(max_length=512,null=True)
+   gender=models.CharField(max_length=512,null=True)
+   debut=models.CharField(max_length=512,null=True)
+   height=models.CharField(max_length=512,null=True)
+   weight=models.CharField(max_length=512,null=True)
+   plays=models.CharField(max_length=512,null=True)
+   grip=models.CharField(max_length=512,null=True)
+   rank=models.CharField(max_length=512,null=True)
+   bestrank=models.CharField(max_length=512,null=True)
+   singlesplayed=models.CharField(max_length=512,null=True)
+   singleswon=models.CharField(max_length=512,null=True)
+   singleslost=models.CharField(max_length=512,null=True)
+   doublesplayed=models.CharField(max_length=512,null=True)
+   doubleswon=models.CharField(max_length=512,null=True)
+   doubleslost=models.CharField(max_length=512,null=True)
+   totalplayed=models.CharField(max_length=512,null=True)
+   totalwn=models.CharField(max_length=512,null=True)
+   totallost=models.CharField(max_length=512,null=True)
+
+
