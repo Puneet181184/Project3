4813c5b3a7876be6529d25ec9bf9c6a7b65679f5
Puneet181184 <chemical.puneet@gmail.com>
2021-10-07 08:50:53 +0530

    added other functions for tabletennis app

--- tabletennis_app/urls.py
+++ tabletennis_app/urls.py
@@ -5,4 +5,17 @@ app_name="tabletennis_app"
 urlpatterns=[
   re_path(r'home/',views.home,name='home'),
   re_path(r'player/',views.player,name='player'),
+  re_path(r'about/',views.about,name='about'),
+  re_path(r'details/',views.details,name='details'),
+  re_path(r'careerstats/',views.careerstats,name='careerstats'),
+  re_path(r'singlesstats/',views.singlesstats,name='singlesstats'),
+  re_path(r'doublesstats/',views.doublesstats,name='doublesstats'),
+  re_path(r'totalstats/',views.totalstats,name='totalstats'),
+  
+  
+
+
+
+
+
   ]
--- tabletennis_app/views.py
+++ tabletennis_app/views.py
@@ -7,3 +7,28 @@ def player(request):
     players_list=tabletennis_db.objects.order_by("name")
     tabletennis_dict={"player":players_list}
     return render(request,"tabletennis_app/player.html",context=tabletennis_dict)			
+def about(request):
+    players_list=tabletennis_db.objects.order_by("name")
+    tabletennis_dict={"player":players_list}
+    return render(request,"tabletennis_app/about.html",context=tabletennis_dict) 
+def details(request):
+    players_list=tabletennis_db.objects.order_by("name")
+    tabletennis_dict={"player":players_list}
+    return render(request,"tabletennis_app/details.html",context=tabletennis_dict)
+def careerstats(request):
+    players_list=tabletennis_db.objects.order_by("name")
+    tabletennis_dict={"player":players_list}
+    return render(request,"tabletennis_app/careerstats.html",context=tabletennis_dict) 
+def singlesstats(request):
+    players_list=tabletennis_db.objects.order_by("name")
+    tabletennis_dict={"player":players_list}
+    return render(request,"tabletennis_app/singlesstats.html",context=tabletennis_dict)
+def doublesstats(request):
+    players_list=tabletennis_db.objects.order_by("name")
+    tabletennis_dict={"player":players_list}
+    return render(request,"tabletennis_app/doublesstats.html",context=tabletennis_dict)
+def totalstats(request):
+    players_list=tabletennis_db.objects.order_by("name")
+    tabletennis_dict={"player":players_list}
+    return render(request,"tabletennis_app/totalstats.html",context=tabletennis_dict)                                                      
+
