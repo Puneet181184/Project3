cb749a608673a8b409989e1865e666f63eb4ab03
Puneet181184 <chemical.puneet@gmail.com>
2021-10-15 09:10:50 +0530

    added other functions to tabletennis app

--- tabletennis_app/forms.py
+++ tabletennis_app/forms.py
@@ -5,3 +5,31 @@ class playerform(forms.ModelForm):
       class Meta():
          model=tabletennis_db
          fields=["name","age","dob","country"]
+class aboutform(forms.ModelForm):
+      class Meta():
+         model=tabletennis_db
+         fields=["name","gender","rank","bestrank"]
+class detailsform(forms.ModelForm):
+      class Meta():
+         model=tabletennis_db
+         fields=["name","debut","height","weight"] 
+class careerstatsform(forms.ModelForm):
+      class Meta():
+         model=tabletennis_db
+         fields=["name","plays","grip"] 
+class singlesstatsform(forms.ModelForm):
+      class Meta():
+         model=tabletennis_db
+         fields=["name","singlesplayed","singleswon","singleslost"] 
+class doublesstatsform(forms.ModelForm):
+      class Meta():
+         model=tabletennis_db
+         fields=["name","doublesplayed","doubleswon","doubleslost"]
+class totalstatsform(forms.ModelForm):
+      class Meta():
+         model=tabletennis_db
+         fields=["name","totalplayed","totalwon","totallost"] 
+
+
+
+
--- tabletennis_app/views.py
+++ tabletennis_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from tabletennis_app.models import tabletennis_db
+from tabletennis_app.forms import playerform
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"tabletennis_app/home.html")
@@ -47,3 +48,92 @@ def form_player(request):
       else:
          print("error form invalid")      
     return render(request,"tabletennis_app/form_player.html",{"form":form})   
+def form_about(request):
+    form=aboutform()
+    if request.method=="POST":
+      form=aboutform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         gender=form.cleaned_data["gender"]
+         rank=form.cleaned_data["rank"]
+         bestrank=form.cleaned_data["bestrank"]
+         defaults={"gender":gender,"rank":rank,"bestrank":bestrank}
+         obj,created=tabletennis_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"tabletennis_app/submit_about.html")
+      else:
+         print("error form invalid")      
+    return render(request,"tabletennis_app/form_about.html",{"form":form})
+def form_details(request):
+    form=detailsform()
+    if request.method=="POST":
+      form=detailsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         debut=form.cleaned_data["debut"]
+         height=form.cleaned_data["height"]
+         weight=form.cleaned_data["weight"]
+         defaults={"debut":debut,"height":height,"weight":weight}
+         obj,created=tabletennis_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"tabletennis_app/submit_details.html")
+      else:
+         print("error form invalid")      
+    return render(request,"tabletennis_app/form_details.html",{"form":form}) 
+def form_careerstats(request):
+    form=careerstatsform()
+    if request.method=="POST":
+      form=careerstatsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         plays=form.cleaned_data["plays"]
+         grip=form.cleaned_data["grip"]
+         defaults={"plays":plays,"grip":grip}
+         obj,created=tabletennis_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"tabletennis_app/submit_careerstats.html")
+      else:
+         print("error form invalid")      
+    return render(request,"tabletennis_app/form_careerstats.html",{"form":form})       
+def form_singlesstats(request):
+    form=singlesstatsform()
+    if request.method=="POST":
+      form=singlesstatsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         singlesplayed=form.cleaned_data["singlesplayed"]
+         singleswon=form.cleaned_data["singleswon"]
+         singleslost=form.cleaned_data["singleslost"]
+         defaults={"singlesplayed":singlesplayed,"singleswon":singleswon,"singleslost":singleslost}
+         obj,created=tabletennis_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"tabletennis_app/submit_singlesstats.html")
+      else:
+         print("error form invalid")      
+    return render(request,"tabletennis_app/form_singlesstats.html",{"form":form})
+def form_doublesstats(request):
+    form=doublesstatsform()
+    if request.method=="POST":
+      form=doublesstatsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         doublesplayed=form.cleaned_data["doublesplayed"]
+         doubleswon=form.cleaned_data["doubleswon"]
+         doublleslost=form.cleaned_data["doubleslost"]
+         defaults={"doublesplayed":doublesplayed,"doubleswon":doubleswon,"doubleslost":doubleslost}
+         obj,created=tabletennis_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"tabletennis_app/submit_doublesstats.html")
+      else:
+         print("error form invalid")      
+    return render(request,"tabletennis_app/form_doublesstats.html",{"form":form}) 
+def form_totalstats(request):
+    form=totalstatsform()
+    if request.method=="POST":
+      form=totalstatsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         totalplayed=form.cleaned_data["totalplayed"]
+         totalwon=form.cleaned_data["totalwon"]
+         totallost=form.cleaned_data["totallost"]
+         defaults={"totalplayed":totalplayed,"totalwon":totalwon,"totallost":totallost}
+         obj,created=tabletennis_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"tabletennis_app/submit_totalstats.html")
+      else:
+         print("error form invalid")      
+    return render(request,"tabletennis_app/form_totalstats.html",{"form":form})       
