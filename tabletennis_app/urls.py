b040f8ef8481900e449e14bdcc4676cc20a87b65
Puneet181184 <chemical.puneet@gmail.com>
2021-10-18 09:00:18 +0530

    added other form player files for tabletennis app

--- tabletennis_app/urls.py
+++ tabletennis_app/urls.py
@@ -12,10 +12,13 @@ urlpatterns=[
   re_path(r'doublesstats/',views.doublesstats,name='doublesstats'),
   re_path(r'totalstats/',views.totalstats,name='totalstats'),
   re_path(r'form_p/',views.form_player,name='form_player'),
+  re_path(r'form_a/',views.form_about,name='form_about'),
+  re_path(r'form_d/',views.form_details,name='form_details'),
+  re_path(r'form_c/',views.form_careerstats,name='form_careerstats'),
+  re_path(r'form_s/',views.form_singlesstats,name='form_singlesstats'),
+  re_path(r'form_do/',views.form_doublesstats,name='form_doublesstats'),
+  re_path(r'form_t/',views.form_totalstats,name='form_totalstats'),
+  
   
 
-
-
-
-
-  ]
+ ]
