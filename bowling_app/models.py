4a349ca7ce2bd0a15acb06470be28d2153fcc6cf
Puneet181184 <chemical.puneet@gmail.com>
2021-11-04 08:54:27 +0530

    added player function to bowling app

--- bowling_app/models.py
+++ bowling_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class  bowling_db(models.Model):
+   name=models.CharField(max_length=512,null=True)
+   age=models.CharField(max_length=512,null=True)
+   country=models.CharField(max_length=512,null=True)
+   gender=models.CharField(max_length=512,null=True)
+   city=models.CharField(max_length=512,null=True)
+   state=models.CharField(max_length=512,null=True)
+   debut=models.CharField(max_length=512,null=True)
+   bowls=models.CharField(max_length=512,null=True)
+   events=models.CharField(max_length=512,null=True)
+   championships=models.CharField(max_length=512,null=True)
+   matches=models.CharField(max_length=512,null=True)
+   cashes=models.CharField(max_length=512,null=True)
+   titles=models.CharField(max_length=512,null=True)
+   average=models.CharField(max_length=512,null=True)
--- bowling_app/views.py
+++ bowling_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"bowling_app/home.html")
+def player(request):
+    players_list=bowling_db.objects.order_by("name")
+    bowling_dict={"player":players_list}
+    return render(request,"bowling_app/player.html",context=bowling_dict)		
