750ea9c8b540ddb7e23cdb34eba84892e4e40e72
Puneet181184 <chemical.puneet@gmail.com>
2021-12-07 08:51:12 +0530

    added player function to weightlifting app

--- weightlifting_app/models.py
+++ weightlifting_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class  weightlifting_db(models.Model):
+   name=models.CharField(max_length=512,null=True)
+   age=models.CharField(max_length=512,null=True)
+   dob=models.CharField(max_length=512,null=True)
+   country=models.CharField(max_length=512,null=True)
+   gender=models.CharField(max_length=512,null=True)
+   category=models.CharField(max_length=512,null=True)
+   rank=models.CharField(max_length=512,null=True)
+   height=models.CharField(max_length=512,null=True)
+   weight=models.CharField(max_length=512,null=True)
+   snatch=models.CharField(max_length=512,null=True)
+   jerk=models.CharField(max_length=512,null=True)
+   total=models.CharField(max_length=512,null=True)
+   
+   
+   
+
+
--- weightlifting_app/views.py
+++ weightlifting_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from weightlifting_app.models import weightlifting_db
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"weightlifting_app/home.html")
+def player(request):
+    players_list=weightlifting_db.objects.order_by("name")
+    weightlifting_dict={"player":players_list}
+    return render(request,"weightlifting_app/player.html",context=weightlifting_dict)		
