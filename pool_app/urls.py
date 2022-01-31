3f9c52c99823cbf730620cdb24102bbe4cd42c84
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-01-31 09:15:15 +0530

    added player function to pool app

--- pool_app/urls.py
+++ pool_app/urls.py
@@ -5,4 +5,8 @@ app_name="pool_app"
 urlpatterns=[
   re_path(r'home/',views.home,name='home'),
   re_path(r'player/',views.player,name='player'),
+  re_path(r'about/',views.about,name='about'),
+  re_path(r'details/',views.details,name='details'),
+  re_path(r'pointstats/',views.pointstats,name='pointstats'),
+  re_path(r'careerstats/',views.careerstats,name='careerstats'),
   ]
--- pool_app/views.py
+++ pool_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from pool_app.models import pool_db
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"pool_app/home.html")
@@ -7,3 +7,19 @@ def player(request):
     players_list=pool_db.objects.order_by("name")
     pool_dict={"player":players_list}
     return render(request,"pool_app/player.html",context=pool_dict)		
+def about(request):
+    players_list=pool_db.objects.order_by("name")
+    pool_dict={"player":players_list}
+    return render(request,"pool_app/about.html",context=pool_dict)
+def details(request):
+    players_list=pool_db.objects.order_by("name")
+    pool_dict={"player":players_list}
+    return render(request,"pool_app/details.html",context=pool_dict)
+def pointstats(request):
+    players_list=pool_db.objects.order_by("name")
+    pool_dict={"player":players_list}
+    return render(request,"pool_app/pointstats.html",context=pool_dict)	
+def careerstats(request):
+    players_list=pool_db.objects.order_by("name")
+    pool_dict={"player":players_list}
+    return render(request,"pool_app/careerstats.html",context=pool_dict)		    	    		    		    
