a16412a1a806b59b57d96c4a3b813344b87c7932
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-02-17 08:55:30 +0530

    added player function to archery app

--- archery_app/models.py
+++ archery_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class  archery_db(models.Model):
+   name=models.CharField(max_length=512,null=True)
+   age=models.CharField(max_length=512,null=True)
+   dob=models.CharField(max_length=512,null=True)
+   country=models.CharField(max_length=512,null=True)
+   debut=models.CharField(max_length=512,null=True)
+   gender=models.CharField(max_length=512,null=True)
+   rank=models.CharField(max_length=512,null=True)
+   arrowlength=models.CharField(max_length=512,null=True)
+   drawweight=models.CharField(max_length=512,null=True)
+   averagearrow=models.CharField(max_length=512,null=True)
+   worldcupgold=models.CharField(max_length=512,null=True)
+   worldcupsilver=models.CharField(max_length=512,null=True)
+   worldcupbronze=models.CharField(max_length=512,null=True)
+   
+   
+   
+
+
--- archery_app/views.py
+++ archery_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from archery_app.models import archery_db
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"archery_app/home.html")
+def player(request):
+    players_list=archery_db.objects.order_by("name")
+    archery_dict={"player":players_list}
+    return render(request,"archery_app/player.html",context=archery_dict)		
