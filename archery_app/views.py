5aaaa41a97cf6bf612651132a9ac35d1a4b12c42
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-02-21 08:40:48 +0530

    added other functions for archery app

--- archery_app/views.py
+++ archery_app/views.py
@@ -7,3 +7,19 @@ def player(request):
     players_list=archery_db.objects.order_by("name")
     archery_dict={"player":players_list}
     return render(request,"archery_app/player.html",context=archery_dict)		
+def about(request):
+    players_list=archery_db.objects.order_by("name")
+    archery_dict={"player":players_list}
+    return render(request,"archery_app/about.html",context=archery_dict)		    
+def details(request):
+    players_list=archery_db.objects.order_by("name")
+    archery_dict={"player":players_list}
+    return render(request,"archery_app/details.html",context=archery_dict)		    
+def gamestats(request):
+    players_list=archery_db.objects.order_by("name")
+    archery_dict={"player":players_list}
+    return render(request,"archery_app/gamestats.html",context=archery_dict)	
+def pointstats(request):
+    players_list=archery_db.objects.order_by("name")
+    archery_dict={"player":players_list}
+    return render(request,"archery_app/pointstats.html",context=archery_dict)		    	    
