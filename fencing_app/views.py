ca4e5a16a89bdbcdc984a7eafd81973d8ab25417
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-03-21 08:45:47 +0530

    added other functions to fencing app

--- fencing_app/views.py
+++ fencing_app/views.py
@@ -7,3 +7,19 @@ def player(request):
     players_list=fencing_db.objects.order_by("name")
     fencing_dict={"player":players_list}
     return render(request,"fencing_app/player.html",context=fencing_dict)		
+def about(request):
+    players_list=fencing_db.objects.order_by("name")
+    fencing_dict={"player":players_list}
+    return render(request,"fencing_app/about.html",context=fencing_dict)
+def details(request):
+    players_list=fencing_db.objects.order_by("name")
+    fencing_dict={"player":players_list}
+    return render(request,"fencing_app/details.html",context=fencing_dict)	
+def gamestats(request):
+    players_list=fencing_db.objects.order_by("name")
+    fencing_dict={"player":players_list}
+    return render(request,"fencing_app/gamestats.html",context=fencing_dict)
+def pointstats(request):
+    players_list=fencing_db.objects.order_by("name")
+    fencing_dict={"player":players_list}
+    return render(request,"fencing_app/pointstats.html",context=fencing_dict)		    		    	    
