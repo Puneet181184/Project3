e2770074d9b4b4c54e1ca8ae6a222e715d626e37
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-03-25 09:03:38 +0530

    added other functions to views.py in fencing app

--- fencing_app/forms.py
+++ fencing_app/forms.py
@@ -5,3 +5,19 @@ class playerform(forms.ModelForm):
       class Meta():
          model=fencing_db
          fields=["name","age","dob","country"]
+class aboutform(forms.ModelForm):
+      class Meta():
+         model=fencing_db
+         fields=["name","debut","gender"] 
+class detailsform(forms.ModelForm):
+      class Meta():
+         model=fencing_db
+         fields=["name","rank","style"]   
+class gamestatsform(forms.ModelForm):
+      class Meta():
+         model=fencing_db
+         fields=["name","hand"] 
+class pointstatsform(forms.ModelForm):
+      class Meta():
+         model=fencing_db
+         fields=["name","points","worldcupgold","worldcupsilver","worldcupbronze"]                               
--- fencing_app/views.py
+++ fencing_app/views.py
@@ -39,3 +39,60 @@ def form_player(request):
       else:
          print("error form invalid")      
     return render(request,"fencing_app/form_player.html",{"form":form})   
+def form_about(request):
+    form=aboutform()
+    if request.method=="POST":
+      form=aboutform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         debut=form.cleaned_data["debut"]
+         gender=form.cleaned_data["gender"]
+         defaults={"debut":debut,"gender":gender}
+         obj,created=fencing_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"fencing_app/submit_about.html")
+      else:
+         print("error form invalid")      
+    return render(request,"fencing_app/form_about.html",{"form":form})  
+def form_details(request):
+    form=detailsform()
+    if request.method=="POST":
+      form=detailsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         rank=form.cleaned_data["rank"]
+         style=form.cleaned_data["style"]
+         defaults={"rank":rank,"style":style}
+         obj,created=fencing_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"fencing_app/submit_details.html")
+      else:
+         print("error form invalid")      
+    return render(request,"fencing_app/form_details.html",{"form":form})  
+def form_gamestats(request):
+    form=gamestatsform()
+    if request.method=="POST":
+      form=gamestatsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         hand=form.cleaned_data["hand"]
+         defaults={"hand":hand}
+         obj,created=fencing_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"fencing_app/submit_gamestats.html")
+      else:
+         print("error form invalid")      
+    return render(request,"fencing_app/form_gamestats.html",{"form":form})  
+def form_pointstats(request):
+    form=pointstatsform()
+    if request.method=="POST":
+      form=pointstatsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         points=form.cleaned_data["points"]
+         worldcupgold=form.cleaned_data["worldcupgold"]
+         worldcupsilver=form.cleaned_data["worldcupsilver"]
+         worldcupbronze=form.cleaned_data["worldcupbronze"]
+         defaults={"points":points,"worldcupgold":worldcupgold,"worldcupsilver":worldcupsilver,"worldcupbronze":worldcupbronze}
+         obj,created=fencing_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"fencing_app/submit_pointstats.html")
+      else:
+         print("error form invalid")      
+    return render(request,"fencing_app/form_pointstats.html",{"form":form})                      
