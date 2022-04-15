a0cc39612447f512778d0aeb48a25fdcd25077ca
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-04-15 08:53:38 +0530

    added other functions to views.py in cycling app

--- cycling_app/urls.py
+++ cycling_app/urls.py
@@ -4,4 +4,9 @@ from django.urls import re_path,path
 app_name="cycling_app"
 urlpatterns=[
   re_path(r'home/',views.home,name='home'),
+  re_path(r'player/',views.player,name='player'),
+  re_path(r'about/',views.about,name='about'),
+  re_path(r'details/',views.details,name='details'),
+  re_path(r'gamestats/',views.gamestats,name='gamestats'),
+  re_path(r'pointstats/',views.pointstats,name='pointstats'),
   ]
--- cycling_app/views.py
+++ cycling_app/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-
+from cycling_app.models import cycling_db
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"cycling_app/home.html")
+def player(request):
+    players_list=cycling_db.objects.order_by("name")
+    cycling_dict={"player":players_list}
+    return render(request,"cycling_app/player.html",context=cycling_dict)		
+def about(request):
+    players_list=cycling_db.objects.order_by("name")
+    cycling_dict={"player":players_list}
+    return render(request,"cycling_app/about.html",context=cycling_dict)		    
+def details(request):
+    players_list=cycling_db.objects.order_by("name")
+    cycling_dict={"player":players_list}
+    return render(request,"cycling_app/details.html",context=cycling_dict)		    
+def gamestats(request):
+    players_list=cycling_db.objects.order_by("name")
+    cycling_dict={"player":players_list}
+    return render(request,"cycling_app/gamestats.html",context=cycling_dict)	
+def pointstats(request):
+    players_list=cycling_db.objects.order_by("name")
+    cycling_dict={"player":players_list}
+    return render(request,"cycling_app/pointstats.html",context=cycling_dict)
