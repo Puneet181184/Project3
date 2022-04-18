399c6cfd39c2df56e8ede2abe33fd7d8411ef74c
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-04-18 09:04:44 +0530

    added player html to cycling app

--- cycling_app/models.py
+++ cycling_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
 # Create your models here.
+class  cycling_db(models.Model):
+   name=models.CharField(max_length=512,null=True)
+   age=models.CharField(max_length=512,null=True)
+   dob=models.CharField(max_length=512,null=True)
+   country=models.CharField(max_length=512,null=True)
+   debut=models.CharField(max_length=512,null=True)
+   gender=models.CharField(max_length=512,null=True)
+   rank=models.CharField(max_length=512,null=True)
+   height=models.CharField(max_length=512,null=True)
+   weight=models.CharField(max_length=512,null=True)
+   points=models.CharField(max_length=512,null=True)
+   wins=models.CharField(max_length=512,null=True)
+   grandtours=models.CharField(max_length=512,null=True)
+   classics=models.CharField(max_length=512,null=True)
+   
+   
