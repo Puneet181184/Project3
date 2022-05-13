0f5c512d6053b051f2c02dcfdbb27e80cd081797
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-05-13 08:01:27 +0530

    added models.py to darts app

--- darts_app/models.py
+++ darts_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class  darts_db(models.Model):
+   name=models.CharField(max_length=512,null=True)
+   age=models.CharField(max_length=512,null=True)
+   dob=models.CharField(max_length=512,null=True)
+   country=models.CharField(max_length=512,null=True)
+   gender=models.CharField(max_length=512,null=True)
+   rank=models.CharField(max_length=512,null=True)
+   height=models.CharField(max_length=512,null=True)
+   darts=models.CharField(max_length=512,null=True)
+   titles=models.CharField(max_length=512,null=True)
+   finishes=models.CharField(max_length=512,null=True)
+  
+   
+   
+   
+
+
+
--- darts_app/views.py
+++ darts_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"darts_app/home.html")
+def player(request):
+    players_list=darts_db.objects.order_by("name")
+    darts_dict={"player":players_list}
+    return render(request,"darts_app/player.html",context=darts_dict)	
