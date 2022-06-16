cb9c5da374c2afa458bb09c51478e08326d2e8ce
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-06-16 08:22:29 +0530

    added player function in views.py in karate app

--- karate_app/models.py
+++ karate_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class  karate_db(models.Model):
+   name=models.CharField(max_length=512,null=True)
+   age=models.CharField(max_length=512,null=True)
+   country=models.CharField(max_length=512,null=True)
+   gender=models.CharField(max_length=512,null=True)
+   rank=models.CharField(max_length=512,null=True)
+   category=models.CharField(max_length=512,null=True)
+   worldgold=models.CharField(max_length=512,null=True)
+   worldsilver=models.CharField(max_length=512,null=True)
+   worldbronze=models.CharField(max_length=512,null=True)
+   
+   
+   
+
+
--- karate_app/views.py
+++ karate_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"karate_app/home.html")
+def player(request):
+    players_list=karate_db.objects.order_by("name")
+    karate_dict={"player":players_list}
+    return render(request,"karate_app/player.html",context=karate_dict)		
