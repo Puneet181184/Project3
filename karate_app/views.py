d680349b7822b2994a935364c8a56f15682a3489
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-06-22 08:17:22 +0530

    added other function in views.py in karate app

--- karate_app/views.py
+++ karate_app/views.py
@@ -8,3 +8,15 @@ def player(request):
     players_list=karate_db.objects.order_by("name")
     karate_dict={"player":players_list}
     return render(request,"karate_app/player.html",context=karate_dict)		
+def about(request):
+    players_list=karate_db.objects.order_by("name")
+    karate_dict={"player":players_list}
+    return render(request,"karate_app/about.html",context=karate_dict)	
+def details(request):
+    players_list=karate_db.objects.order_by("name")
+    karate_dict={"player":players_list}
+    return render(request,"karate_app/details.html",context=karate_dict)
+def pointstats(request):
+    players_list=karate_db.objects.order_by("name")
+    karate_dict={"player":players_list}
+    return render(request,"karate_app/pointstats.html",context=karate_dict)		    		    	    
