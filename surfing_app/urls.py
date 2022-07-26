1e9653f034829c79afbb8a406cc8eaaf6d464b06
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-07-26 08:12:11 +0530

    added player function to views.py in surfing app

--- surfing_app/urls.py
+++ surfing_app/urls.py
@@ -4,4 +4,5 @@ from django.urls import re_path,path
 app_name="surfing_app"
 urlpatterns=[
   re_path(r'home/',views.home,name='home'),
+  re_path(r'player/',views.player,name='player'),
   ]
--- surfing_app/views.py
+++ surfing_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"surfing_app/home.html")
+def player(request):
+    players_list=surfing_db.objects.order_by("name")
+    surfing_dict={"player":players_list}
+    return render(request,"surfing_app/player.html",context=surfing_dict)	
