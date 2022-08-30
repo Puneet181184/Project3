b6dc8434ae229a65e38f999ed46cd6857f49daa7
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-08-30 07:46:03 +0530

    added player function to taekwondo app

--- taekwondo_app/views.py
+++ taekwondo_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from taekwondo_app.models import taekwondo_db
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"taekwondo_app/home.html")
+def player(request):
+    players_list=taekwondo_db.objects.order_by("name")
+    taekwondo_dict={"player":players_list}
+    return render(request,"taekwondo_app/player.html",context=taekwondo_dict)	
