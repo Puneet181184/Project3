9ee7cd76cb472ce1491c188939f62a4bc1c0b655
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-09-28 07:41:40 +0530

    added playerv function in views.py in volleyball app

--- volleyball_app/views.py
+++ volleyball_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from volleyball_app.models import volleyball_db
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"volleyball_app/home.html")
+def player(request):
+    players_list=volleyball_db.objects.order_by("name")
+    volleyball_dict={"player":players_list}
+    return render(request,"volleyball_app/player.html",context=volleyball_dict)			
