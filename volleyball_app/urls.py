e995bfb5508092b53128cee29d9615605ac9d64d
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-09-30 07:49:08 +0530

    added other html files to volleyball app

--- volleyball_app/urls.py
+++ volleyball_app/urls.py
@@ -5,5 +5,7 @@ app_name="volleyball_app"
 urlpatterns=[
   re_path(r'home/',views.home,name='home'),
   re_path(r'player/',views.player,name='player'),
-
+  re_path(r'about/',views.about,name='about'),
+  re_path(r'details/',views.details,name='details'),
+  re_path(r'pointstats/',views.pointstats,name='pointstats'),
   ]
--- volleyball_app/views.py
+++ volleyball_app/views.py
@@ -6,4 +6,18 @@ def home(request):
 def player(request):
     players_list=volleyball_db.objects.order_by("name")
     volleyball_dict={"player":players_list}
-    return render(request,"volleyball_app/player.html",context=volleyball_dict)			
+    return render(request,"volleyball_app/player.html",context=volleyball_dict)	
+def about(request):
+    players_list=volleyball_db.objects.order_by("name")
+    volleyball_dict={"player":players_list}
+    return render(request,"volleyball_app/about.html",context=volleyball_dict)			
+def details(request):
+    players_list=volleyball_db.objects.order_by("name")
+    volleyball_dict={"player":players_list}
+    return render(request,"volleyball_app/details.html",context=volleyball_dict)	
+def pointstats(request):
+    players_list=volleyball_db.objects.order_by("name")
+    volleyball_dict={"player":players_list}
+    return render(request,"volleyball_app/pointstats.html",context=volleyball_dict)			
+
+
