4d37ba7b6cecff540bf4d4f40b920e8f4ab4258a
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-11-16 08:28:36 +0530

    added player funcytion in views.py in horseracing app

--- horseracing_app/models.py
+++ horseracing_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class  horseracing_db(models.Model):
+   name=models.CharField(max_length=512,null=True)
+   age=models.CharField(max_length=512,null=True)
+   rank=models.CharField(max_length=512,null=True)
+   horse=models.CharField(max_length=512,null=True)
+   starts=models.CharField(max_length=512,null=True)
+   gold=models.CharField(max_length=512,null=True)
+   silver=models.CharField(max_length=512,null=True)
+   bronze=models.CharField(max_length=512,null=True)
+   
+   
--- horseracing_app/views.py
+++ horseracing_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from horseracing_app.models import horseracing_db
 
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"horseracing_app/home.html")
+def player(request):
+    players_list=horseracing_db.objects.order_by("name")
+    horseracing_dict={"player":players_list}
+    return render(request,"horseracing_app/player.html",context=horseracing_dict)		
