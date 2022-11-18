c9bd9f543f1a25908309f6132c1f8a26493b8a21
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-11-18 09:14:04 +0530

    added other html files to horseracing app

--- horseracing_app/views.py
+++ horseracing_app/views.py
@@ -8,3 +8,15 @@ def player(request):
     players_list=horseracing_db.objects.order_by("name")
     horseracing_dict={"player":players_list}
     return render(request,"horseracing_app/player.html",context=horseracing_dict)		
+def about(request):
+    players_list=horseracing_db.objects.order_by("name")
+    horseracing_dict={"player":players_list}
+    return render(request,"horseracing_app/about.html",context=horseracing_dict)
+def details(request):
+    players_list=horseracing_db.objects.order_by("name")
+    horseracing_dict={"player":players_list}
+    return render(request,"horseracing_app/details.html",context=horseracing_dict)		    		    
+def pointstats(request):
+    players_list=horseracing_db.objects.order_by("name")
+    horseracing_dict={"player":players_list}
+    return render(request,"horseracing_app/pointstats.html",context=horseracing_dict)		    
