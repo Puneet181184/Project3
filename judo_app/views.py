b6e259bfa59883bfe01c8ffc2b9d4f4dc048f018
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2022-12-02 08:37:14 +0530

    added player function in views.py in judo app

--- judo_app/views.py
+++ judo_app/views.py
@@ -3,3 +3,15 @@ from judo_app.models import judo_db
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"judo_app/home.html")
+def player(request):
+    players_list=judo_db.objects.order_by("name")
+    judo_dict={"player":players_list}
+    return render(request,"judo_app/player.html",context=judo_dict)	
+def about(request):
+    players_list=judo_db.objects.order_by("name")
+    judo_dict={"player":players_list}
+    return render(request,"judo_app/about.html",context=judo_dict)	
+def pointstats(request):
+    players_list=judo_db.objects.order_by("name")
+    judo_dict={"player":players_list}
+    return render(request,"judo_app/pointstats.html",context=judo_dict)	        
