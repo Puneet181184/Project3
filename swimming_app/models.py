255de2b2c0cb3faa990f9ff8ad04d7b6d3b6d813
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2023-01-26 07:46:29 +0530

    added models.py to swimmimg app

--- swimming_app/models.py
+++ swimming_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class  swimming_db(models.Model):
+   name=models.CharField(max_length=512,null=True)
+   age=models.CharField(max_length=512,null=True)
+   country=models.CharField(max_length=512,null=True)
+   rank=models.CharField(max_length=512,null=True)
+   medals=models.CharField(max_length=512,null=True)
+   gold=models.CharField(max_length=512,null=True)
+   silver=models.CharField(max_length=512,null=True)
+   bronze=models.CharField(max_length=512,null=True)
+   
--- swimming_app/views.py
+++ swimming_app/views.py
@@ -4,3 +4,7 @@ from swimming_app.models import swimming_db
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"swimming_app/home.html")
+def player(request):
+    players_list=swimming_db.objects.order_by("name")
+    swimming_dict={"player":players_list}
+    return render(request,"swimming_app/player.html",context=swimming_dict)		
