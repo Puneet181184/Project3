6c70a890b1b0ce580e33f0266a1f2433b930d160
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2023-01-30 08:13:44 +0530

    added other functions in views.py in swimming app

--- swimming_app/urls.py
+++ swimming_app/urls.py
@@ -4,5 +4,10 @@ from django.urls import re_path,path
 app_name="swimming_app"
 urlpatterns=[
   re_path(r'home/',views.home,name='home'),
-   re_path(r'player/',views.player,name='player'),
+  re_path(r'player/',views.player,name='player'),
+  re_path(r'about/',views.about,name='about'),
+  re_path(r'details/',views.details,name='details'),
+  re_path(r'pointstats/',views.pointstats,name='pointstats'),
+
+
   ]
--- swimming_app/views.py
+++ swimming_app/views.py
@@ -8,3 +8,15 @@ def player(request):
     players_list=swimming_db.objects.order_by("name")
     swimming_dict={"player":players_list}
     return render(request,"swimming_app/player.html",context=swimming_dict)		
+def about(request):
+    players_list=swimming_db.objects.order_by("name")
+    swimming_dict={"player":players_list}
+    return render(request,"swimming_app/about.html",context=swimming_dict)
+def details(request):
+    players_list=swimming_db.objects.order_by("name")
+    swimming_dict={"player":players_list}
+    return render(request,"swimming_app/details.html",context=swimming_dict)
+def pointstats(request):
+    players_list=swimming_db.objects.order_by("name")
+    swimming_dict={"player":players_list}
+    return render(request,"swimming_app/pointstats.html",context=swimming_dict)		    		    		    
