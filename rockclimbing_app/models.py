eed53a64e167476cde7e44ca29674687c1f76517
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2023-04-14 08:17:37 +0530

    added player function in views.py in rockclimbing app

--- rockclimbing_app/models.py
+++ rockclimbing_app/models.py
@@ -1,3 +1,14 @@
+
 from django.db import models
 
+
 # Create your models here.
+class  rockclimbing_db(models.Model):
+   name=models.CharField(max_length=512,null=True)
+   age=models.CharField(max_length=512,null=True)
+   country=models.CharField(max_length=512,null=True)
+   height=models.CharField(max_length=512,null=True)
+   weight=models.CharField(max_length=512,null=True)
+   rankingboulder=models.CharField(max_length=512,null=True)
+   rankinglead=models.CharField(max_length=512,null=True)
+  
--- rockclimbing_app/views.py
+++ rockclimbing_app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from rockclimbing_app.models import rockclimbing_db
-from archery_app.forms import playerform
-from archery_app.forms import aboutform
-from archery_app.forms import detailsform
-from archery_app.forms import gamestatsform
-from archery_app.forms import pointstatsform
-from archery_app.forms import searchform
+from rockclimbing_app.forms import playerform
+from rockclimbing_app.forms import aboutform
+from rockclimbing_app.forms import detailsform
+from rockclimbing_app.forms import searchform
 
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"rockclimbing_app/home.html")
+def player(request):
+    players_list=rockclimbing_db.objects.order_by("name")
+    rockclimbing_dict={"player":players_list}
+    return render(request,"rockclimbing_app/player.html",context=rockclimbing_dict)
