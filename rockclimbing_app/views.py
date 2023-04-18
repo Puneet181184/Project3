19c1403486f6f188a44f61a6a64fb5dd2966db60
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2023-04-18 07:32:54 +0530

    added other functions in views.py in rockclimbing app

--- rockclimbing_app/views.py
+++ rockclimbing_app/views.py
@@ -12,3 +12,15 @@ def player(request):
     players_list=rockclimbing_db.objects.order_by("name")
     rockclimbing_dict={"player":players_list}
     return render(request,"rockclimbing_app/player.html",context=rockclimbing_dict)
+def about(request):
+    players_list=rockclimbing_db.objects.order_by("name")
+    rockclimbing_dict={"player":players_list}
+    return render(request,"rockclimbing_app/about.html",context=rockclimbing_dict)    
+def details(request):
+    players_list=rockclimbing_db.objects.order_by("name")
+    rockclimbing_dict={"player":players_list}
+    return render(request,"rockclimbing_app/details.html",context=rockclimbing_dict)    
+def pointstats(request):
+    players_list=rockclimbing_db.objects.order_by("name")
+    rockclimbing_dict={"player":players_list}
+    return render(request,"rockclimbing_app/pointstats.html",context=rockclimbing_dict)    
