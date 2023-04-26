dff700064503a0c0af0557e3bed51b2c62405439
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2023-04-26 10:02:37 +0530

    added all search function in views.py in rockclimbing app

--- rockclimbing_app/admin.py
+++ rockclimbing_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from rockclimbing_app.models import rockclimbing_db
+admin.site.register(rockclimbing_db)
 
-# Register your models here.
+
--- rockclimbing_app/views.py
+++ rockclimbing_app/views.py
@@ -61,4 +61,47 @@ def form_pointstats(request):
          return render(request,"kickboxing_app/submit_pointstats.html")
       else:
          print("error form invalid")      
-    return render(request,"kickboxing_app/form_pointstats.html",{"form":form})                
+    return render(request,"kickboxing_app/form_pointstats.html",{"form":form})  
+def search_player(request):
+    form=searchform()
+    if request.method=="POST":
+      form=searchform(request.POST) 
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         try:
+             my_value=rockclimbing_db.objects.get(name__iexact=name) 
+         except rockclimbing_db.DoesNotExist:
+             return render(request,"rockclimbing_app/error_player.html")
+         return render(request,"rockclimbing_app/result_player.html",context={"player":my_value})
+      else:
+         print(" error form invalid")
+    return render(request,"rockclimbing_app/search_player.html",{"form":form})
+def search_about(request):
+    form=searchform()
+    if request.method=="POST":
+      form=searchform(request.POST) 
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         try:
+             my_value=rockclimbing_db.objects.get(name__iexact=name) 
+         except rockclimbing_db.DoesNotExist:
+             return render(request,"rockclimbing_app/error_about.html")
+         return render(request,"rockclimbing_app/result_about.html",context={"player":my_value})
+      else:
+         print(" error form invalid")
+    return render(request,"rockclimbing_app/search_about.html",{"form":form}) 
+def search_pointstats(request):
+    form=searchform()
+    if request.method=="POST":
+      form=searchform(request.POST) 
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         try:
+             my_value=rockclimbing_db.objects.get(name__iexact=name) 
+         except rockclimbing_db.DoesNotExist:
+             return render(request,"rockclimbing_app/error_pointstats.html")
+         return render(request,"rockclimbing_app/result_pointstats.html",context={"player":my_value})
+      else:
+         print(" error form invalid")
+    return render(request,"rockclimbing_app/search_pointstats.html",{"form":form})      
+    return render(request,"rockclimbing_app/search_player.html",{"form":form})                                                             
