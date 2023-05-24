f9cce66d622037b3a5b0c74280f2299712fbd316
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2023-05-24 08:04:05 +0530

    added player function in views.py in iceskating app

--- iceskating_app/views.py
+++ iceskating_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from iceskating_app.models import iceskating_db
 from iceskating_app.forms import playerform
-from archery_app.forms import aboutform
-from archery_app.forms import detailsform
-from archery_app.forms import gamestatsform
-from archery_app.forms import pointstatsform
+from iceskating_app.forms import aboutform
+from iceskating_app.forms import detailsform
 from archery_app.forms import searchform
 
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"iceskating_app/home.html")
+def player(request):
+    players_list=iceskating_db.objects.order_by("name")
+    iceskating_dict={"player":players_list}
+    return render(request,"iceskating_app/player.html",context=iceskating_dict)			
+
