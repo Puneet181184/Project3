0a0a2613fd9a37415dc98a0911c0e03592f67ba0
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2023-06-01 09:41:24 +0530

    added form player html to iceskating app

--- iceskating_app/urls.py
+++ iceskating_app/urls.py
@@ -8,5 +8,9 @@ urlpatterns=[
   re_path(r'about/',views.about,name='about'),
   re_path(r'details/',views.details,name='details'),
   re_path(r'form_p/',views.form_player,name='form_player'),
-
+  re_path(r'form_a/',views.form_about,name='form_about'),
+  re_path(r'form_d/',views.form_details,name='form_details'),
+  re_path(r'search_p/',views.search_player,name='search_player'),
+  re_path(r'search_a/',views.search_about,name='search_about'),
+  re_path(r'search_d/',views.search_details,name='search_details'),
   ]
--- iceskating_app/views.py
+++ iceskating_app/views.py
@@ -34,3 +34,72 @@ def form_player(request):
       else:
          print("error form invalid")      
     return render(request,"iceskating_app/form_player.html",{"form":form})       
+def form_about(request):
+    form=aboutform()
+    if request.method=="POST":
+      form=aboutform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         height=form.cleaned_data["height"]
+         weight=form.cleaned_data["weight"]
+         defaults={"height":height,"weight":weight}
+         obj,created=iceskating_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"iceskating_app/submit_about.html")
+      else:
+         print("error form invalid")      
+    return render(request,"iceskating_app/form_about.html",{"form":form}) 
+def form_details(request):
+    form=detailsform()
+    if request.method=="POST":
+      form=detailsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         level=form.cleaned_data["level"]
+         defaults={"level":level}
+         obj,created=iceskating_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"iceskating_app/submit_details.html")
+      else:
+         print("error form invalid")      
+    return render(request,"iceskating_app/form_details.html",{"form":form})
+def search_player(request):
+    form=searchform()
+    if request.method=="POST":
+      form=searchform(request.POST) 
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         try:
+             my_value=iceskating_db.objects.get(name__iexact=name) 
+         except iceskating_db.DoesNotExist:
+             return render(request,"iceskating_app/error_player.html")
+         return render(request,"iceskating_app/result_player.html",context={"player":my_value})
+      else:
+         print(" error form invalid")
+    return render(request,"iceskating_app/search_player.html",{"form":form}) 
+def search_about(request):
+    form=searchform()
+    if request.method=="POST":
+      form=searchform(request.POST) 
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         try:
+             my_value=iceskating_db.objects.get(name__iexact=name) 
+         except iceskating_db.DoesNotExist:
+             return render(request,"iceskating_app/error_about.html")
+         return render(request,"iceskating_app/result_about.html",context={"player":my_value})
+      else:
+         print(" error form invalid")
+    return render(request,"iceskating_app/search_about.html",{"form":form}) 
+def search_details(request):
+    form=searchform()
+    if request.method=="POST":
+      form=searchform(request.POST) 
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         try:
+             my_value=iceskating_db.objects.get(name__iexact=name) 
+         except iceskating_db.DoesNotExist:
+             return render(request,"iceskating_app/error_details.html")
+         return render(request,"iceskating_app/result_details.html",context={"player":my_value})
+      else:
+         print(" error form invalid")
+    return render(request,"iceskating_app/search_details.html",{"form":form})               
