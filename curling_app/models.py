955cc9aad630e57d5af31a326eb3d94574c4d779
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2023-06-27 08:00:25 +0530

    added player html to curling app

--- curling_app/models.py
+++ curling_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class  curling_db(models.Model):
+   name=models.CharField(max_length=512,null=True)
+   age=models.CharField(max_length=512,null=True)
+   country=models.CharField(max_length=512,null=True)
+   ranking=models.CharField(max_length=512,null=True)
+   
+   
+   
+   
+
+
+
