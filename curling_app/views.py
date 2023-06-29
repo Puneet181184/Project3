067938f022cd8201255dc29d497b2323a46baf01
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2023-06-29 09:46:12 +0530

    added about.html to curling app

--- curling_app/views.py
+++ curling_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from curling_app.models import curling_db
 from curling_app.forms import playerform
 from curling_app.forms import aboutform
-from curling_app.forms import detailsform
 from curling_app.forms import searchform
 
 def home(request):
@@ -12,3 +11,7 @@ def player(request):
     players_list=curling_db.objects.order_by("name")
     curling_dict={"player":players_list}
     return render(request,"curling_app/player.html",context=curling_dict)
+def about(request):
+    players_list=curling_db.objects.order_by("name")
+    curling_dict={"player":players_list}
+    return render(request,"curling_app/about.html",context=curling_dict)    
