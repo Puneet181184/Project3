e3571673158fa9183ad3d48cbd59422101df4bac
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2023-08-02 16:17:27 +0530

    added player function in views.py in lacrosse app

--- lacrosse_app/views.py
+++ lacrosse_app/views.py
@@ -8,3 +8,7 @@ from lacrosse_app.forms import searchform
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"lacrosse_app/home.html")
+def player(request):
+    players_list=lacrosse_db.objects.order_by("name")
+    lacrosse_dict={"player":players_list}
+    return render(request,"lacrosse_app/player.html",context=lacrosse_dict)		
