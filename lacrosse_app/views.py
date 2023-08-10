1f05b4efbfc1abcae882ff3516912fe15ca53ee0
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2023-08-10 09:34:09 +0530

    added other functions in views.py in lacrosse app

--- lacrosse_app/views.py
+++ lacrosse_app/views.py
@@ -16,7 +16,48 @@ def about(request):
     players_list=lacrosse_db.objects.order_by("name")
     lacrosse_dict={"player":players_list}
     return render(request,"lacrosse_app/about.html",context=lacrosse_dict)     
-def player(request):
+def pointstats(request):
     players_list=lacrosse_db.objects.order_by("name")
     lacrosse_dict={"player":players_list}
     return render(request,"lacrosse_app/pointstats.html",context=lacrosse_dict)         
+def form_player(request):
+    form=playerform()
+    if request.method=="POST":
+      form=playerform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         age=form.cleaned_data["age"]
+         country=form.cleaned_data["country"]
+         defaults={"age":age,"country":country}
+         obj,created=lacrosse_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"lacrosse_app/submit_player.html")
+      else:
+         print("error form invalid")      
+    return render(request,"lacrosse_app/form_player.html",{"form":form})           
+def form_about(request):
+    form=aboutform()
+    if request.method=="POST":
+      form=aboutform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         height=form.cleaned_data["height"]
+         weight=form.cleaned_data["weight"]
+         defaults={"height":height,"weight":weight}
+         obj,created=lacrosse_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"lacrosse_app/submit_about.html")
+      else:
+         print("error form invalid")      
+    return render(request,"lacrosse_app/form_about.html",{"form":form})    
+def form_pointstats(request):
+    form=pointstatsform()
+    if request.method=="POST":
+      form=pointstatsform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         rank=form.cleaned_data["rank"]
+         defaults={"rank":rank}
+         obj,created=lacrosse_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"lacrosse_app/submit_pointstats.html")
+      else:
+         print("error form invalid")      
+    return render(request,"lacrosse_app/form_pointstats.html",{"form":form})               
