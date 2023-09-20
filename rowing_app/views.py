c0bac27440ab8d411aa7c487dd2eb627e5c9cf80
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2023-09-20 08:14:37 +0530

    added form_player function to views.py of rowing app

--- rowing_app/views.py
+++ rowing_app/views.py
@@ -15,3 +15,31 @@ def about(request):
     players_list=rowing_db.objects.order_by("name")
     rowing_dict={"player":players_list}
     return render(request,"rowing_app/about.html",context=rowing_dict)     
+def form_player(request):
+    form=playerform()
+    if request.method=="POST":
+      form=playerform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         age=form.cleaned_data["age"]
+         country=form.cleaned_data["country"]
+         defaults={"age":age,"country":country}
+         obj,created=rowing_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"rowing_app/submit_player.html")
+      else:
+         print("error form invalid")      
+    return render(request,"rowing_app/form_player.html",{"form":form}) 
+def form_about(request):
+    form=aboutform()
+    if request.method=="POST":
+      form=aboutform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         height=form.cleaned_data["height"]
+         weight=form.cleaned_data["weight"]
+         defaults={"height":height,"weight":weight}
+         obj,created=rowing_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"rowing_app/submit_about.html")
+      else:
+         print("error form invalid")      
+    return render(request,"rowing_app/form_about.html",{"form":form})          
