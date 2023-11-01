731f121b3885bb71100e3298340f6138957da9cf
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2023-11-02 05:20:50 +0530

    added player function in views.py of polo app

--- polo_app/views.py
+++ polo_app/views.py
@@ -3,8 +3,12 @@ from polo_app.models import polo_db
 from polo_app.forms import playerform
 from polo_app.forms import aboutform
 from polo_app.forms import detailsform
-from archery_app.forms import searchform
+from polo_app.forms import searchform
 
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request,"polo_app/home.html")
+def player(request):
+    players_list=polo_db.objects.order_by("name")
+    polo_dict={"player":players_list}
+    return render(request,"polo_app/player.html",context=polo_dict)	
