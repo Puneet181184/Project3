2e02ed9c6a1df3922a14baac418b2aecf74832f8
Puneet Agrawal <puneetagrawal@Puneets-MacBook-Pro.local>
2023-11-09 06:40:14 +0530

    added form_player function in views.py in polo app

--- polo_app/views.py
+++ polo_app/views.py
@@ -14,4 +14,32 @@ def player(request):
 def about(request):
     players_list=polo_db.objects.order_by("name")
     polo_dict={"player":players_list}
-    return render(request,"polo_app/about.html",context=polo_dict)     
+    return render(request,"polo_app/about.html",context=polo_dict)
+def form_player(request):
+    form=playerform()
+    if request.method=="POST":
+      form=playerform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         age=form.cleaned_data["age"]
+         country=form.cleaned_data["country"]
+         defaults={"age":age,"country":country}
+         obj,created=polo_db.objects.update_or_create(title=title,defaults=defaults)
+         return render(request,"polo_app/submit_player.html")
+      else:
+         print("error form invalid")      
+    return render(request,"polo_app/form_player.html",{"form":form})            
+def form_about(request):
+    form=aboutform()
+    if request.method=="POST":
+      form=aboutform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         status=form.cleaned_data["status"]
+         points=form.cleaned_data["points"]
+         defaults={"status":status,"points":points}
+         obj,created=polo_db.objects.update_or_create(title=title,defaults=defaults)
+         return render(request,"polo_app/submit_about.html")
+      else:
+         print("error form invalid")      
+    return render(request,"polo_app/form_about.html",{"form":form})                
