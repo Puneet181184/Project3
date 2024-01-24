a7bdd54bc8356bbe89623d7e1b029dae7d09911e
Puneet Agrawal <chemical.puneet@gmail.com>
2024-01-24 08:00:38 +0530

    added form player function to views.py of handball app

--- handball_app/views.py
+++ handball_app/views.py
@@ -10,8 +10,37 @@ def home(request):
 def player(request):
     players_list=handball_db.objects.order_by("name")
     handball_dict={"player":players_list}
-    return render(request,"handball_app/player.html",context=archery_dict)
+    return render(request,"handball_app/player.html",context=handball_dict)
 def about(request):
     players_list=handball_db.objects.order_by("name")
     handball_dict={"player":players_list}
-    return render(request,"handball_app/about.html",context=archery_dict)	    	
+    return render(request,"handball_app/about.html",context=handball_dict)
+def form_player(request):
+    form=playerform()
+    if request.method=="POST":
+      form=playerform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         age=form.cleaned_data["age"]
+         country=form.cleaned_data["country"]
+         defaults={"age":age,"country":country}
+         obj,created=handball_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"handball_app/submit_player.html")
+      else:
+         print("error form invalid")      
+    return render(request,"handball_app/form_player.html",{"form":form}) 
+def form_about(request):
+    form=aboutform()
+    if request.method=="POST":
+      form=aboutform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         height=form.cleaned_data["height"]
+         weight=form.cleaned_data["weight"]
+         position=form.cleaned_data["position"]
+         defaults={"height":height,"weight":weight,"position":position}
+         obj,created=handball_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"handball_app/submit_about.html")
+      else:
+         print("error form invalid")      
+    return render(request,"handball_app/form_about.html",{"form":form})           	    	
