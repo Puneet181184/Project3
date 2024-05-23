9fcca4251e673767e80932a3a9cdc210247f4e01
Puneet Agrawal <chemical.puneet@gmail.com>
2024-05-23 10:11:43 +0530

    added form_player funcction to views.py of skiing app

--- skiing_app/urls.py
+++ skiing_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns=[
   re_path(r'home/',views.home,name='home'),
   re_path(r'player/',views.player,name='player'),
   re_path(r'about/',views.about,name='about'),
-  
+  re_path(r'form_p/',views.form_player,name='form_player'),
+  re_path(r'form_a/',views.form_about,name='form_about'),
   ]
--- skiing_app/views.py
+++ skiing_app/views.py
@@ -15,3 +15,31 @@ def about(request):
     players_list=skiing_db.objects.order_by("name")
     skiing_dict={"player":players_list}
     return render(request,"skiing_app/about.html",context=skiing_dict)     
+def form_player(request):
+    form=playerform()
+    if request.method=="POST":
+      form=playerform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         age=form.cleaned_data["age"]
+         dob=form.cleaned_data["dob"]
+         defaults={"age":age,"dob":dob}
+         obj,created=skiing_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"skiing_app/submit_player.html")
+      else:
+         print("error form invalid")      
+    return render(request,"skiing_app/form_player.html",{"form":form})    
+def form_about(request):
+    form=aboutform()
+    if request.method=="POST":
+      form=aboutform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         height=form.cleaned_data["height"]
+         skitype=form.cleaned_data["skitype"]
+         defaults={"height":height,"skitype":skitype}
+         obj,created=skiing_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"skiing_app/submit_about.html")
+      else:
+         print("error form invalid")      
+    return render(request,"skiing_app/form_about.html",{"form":form})        
