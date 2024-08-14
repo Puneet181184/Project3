0b6ee2419158676c5ec5c3418e88da8a6a820877
Puneet Agrawal <chemical.puneet@gmail.com>
2024-08-14 07:46:46 +0530

    added form player function to views.py og diving app

--- diving_app/views.py
+++ diving_app/views.py
@@ -15,3 +15,31 @@ def about(request):
     players_list=diving_db.objects.order_by("name")
     diving_dict={"player":players_list}
     return render(request,"diving_app/about.html",context=diving_dict)	
+def form_player(request):
+    form=playerform()
+    if request.method=="POST":
+      form=playerform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         dob=form.cleaned_data["dob"]
+         country=form.cleaned_data["country"]
+         defaults={"dob":dob,"country":country}
+         obj,created=diving_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"diving_app/submit_player.html")
+      else:
+         print("error form invalid")      
+    return render(request,"diving_app/form_player.html",{"form":form})         
+def form_about(request):
+    form=aboutform()
+    if request.method=="POST":
+      form=aboutform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         gender=form.cleaned_data["gender"]
+         discipline=form.cleaned_data["discipline"]
+         defaults={"gender":gender,"discipline":discipline}
+         obj,created=diving_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"diving_app/submit_about.html")
+      else:
+         print("error form invalid")      
+    return render(request,"diving_app/form_about.html",{"form":form})             
