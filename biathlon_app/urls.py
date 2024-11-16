5d3dff4720f30b8682a35c2b95830a28e0dd6d91
Puneet Agrawal <chemical.puneet@gmail.com>
2024-11-16 07:39:12 +0530

    added form player function to views.py of biathlon app

--- biathlon_app/urls.py
+++ biathlon_app/urls.py
@@ -6,4 +6,8 @@ urlpatterns=[
   re_path(r'home/',views.home,name='home'),
   re_path(r'player/',views.player,name='player'),
   re_path(r'about/',views.about,name='about'),
+  re_path(r'form_p/',views.form_player,name='form_player'),
+  re_path(r'form_a/',views.form_about,name='form_about'),
+  re_path(r'search_p/',views.search_player,name='search_player'),
+  re_path(r'search_a/',views.search_about,name='search_about'),
   ]
--- biathlon_app/views.py
+++ biathlon_app/views.py
@@ -15,3 +15,60 @@ def about(request):
     players_list=biathlon_db.objects.order_by("name")
     biathlon_dict={"player":players_list}
     return render(request,"biathlon_app/about.html",context=biathlon_dict)	
+def form_player(request):
+    form=playerform()
+    if request.method=="POST":
+      form=playerform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         dob=form.cleaned_data["dob"]
+         country=form.cleaned_data["country"]
+         defaults={"dob":dob,"country":country}
+         obj,created=biathlon_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"biathlon_app/submit_player.html")
+      else:
+         print("error form invalid")      
+    return render(request,"biathlon_app/form_player.html",{"form":form})     
+def form_about(request):
+    form=aboutform()
+    if request.method=="POST":
+      form=aboutform(request.POST)
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         gender=form.cleaned_data["gender"]
+         rank=form.cleaned_data["rank"]
+         points=form.cleaned_data["points"]
+         defaults={"gender":gender,"rank":rank,"points":points}
+         obj,created=biathlon_db.objects.update_or_create(name=name,defaults=defaults)
+         return render(request,"biathlon_app/submit_about.html")
+      else:
+         print("error form invalid")      
+    return render(request,"biathlon_app/form_about.html",{"form":form})     
+def search_player(request):
+    form=searchform()
+    if request.method=="POST":
+      form=searchform(request.POST) 
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         try:
+             my_value=biathlon_db.objects.get(name__iexact=name) 
+         except biathlon_db.DoesNotExist:
+             return render(request,"biathlon_app/error_player.html")
+         return render(request,"biathlon_app/result_player.html",context={"player":my_value})
+      else:
+         print(" error form invalid")
+    return render(request,"biathlon_app/search_player.html",{"form":form})
+def search_about(request):
+    form=searchform()
+    if request.method=="POST":
+      form=searchform(request.POST) 
+      if form.is_valid():
+         name=form.cleaned_data["name"]
+         try:
+             my_value=biathlon_db.objects.get(name__iexact=name) 
+         except archery_db.DoesNotExist:
+             return render(request,"biathlon_app/error_about.html")
+         return render(request,"biathlon_app/result_about.html",context={"player":my_value})
+      else:
+         print(" error form invalid")
+    return render(request,"biathlon_app/search_about.html",{"form":form})    
